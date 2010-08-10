43473e22d8827c8ac3407bc7b75b7f0ef4bcf11f
yuiseki <yuiseki@gmail.com>
2010-08-11 04:16:27 +0900

    add api access cache

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader'
 require 'haml'
 require "google_spreadsheet"
 require "pit"
+require "fastercsv"
 
 configure do
     set :account, Pit.get("google.com", :require => {
@@ -14,8 +15,17 @@ end
 
 helpers do
   def get_gdocs
-    # TODO キャッシュがあるか確認
-    if true
+    # キャッシュがあるか確認
+    cache_path = 'tmp/gdocs.csv'
+    if File.exist?(cache_path)
+      # csvからrowsをかえす
+      rows = FasterCSV.read(cache_path)
+      # 期限が切れていたら削除
+      cache_elapse = Time.now - File::mtime(cache_path)
+      File.delete(cache_path) if cache_elapse > 3200
+      return rows
+    else
+      puts "gdocs api access"
       # タイムアウトしてたらAPI叩く
       session = GoogleSpreadsheet.login(options.account["username"], options.account["password"])
       ws = session.spreadsheet_by_key("tZ034W7OBe2_nmdh5XVw8Tg").worksheets[0]
@@ -25,7 +35,12 @@ helpers do
       rows.shift
       # 公表日時前のものは削除
       rows.delete_if{|row| Time.parse(row[0]) > Time.now }
-      # TODO キャッシュを残す
+      # キャッシュを残す
+      FasterCSV.open(cache_path, "w") do |csv|
+        rows.each do |row|
+          csv << row
+        end
+      end
       return rows
     end
   end
@@ -35,6 +50,9 @@ get '/' do
   @title = "荒川智則.jp"
   @result = get_gdocs
   haml :index
+end
+
+get '/rss' do
 end
 
 # 背景画像をランダムに選んでリダイレクトする
@@ -51,9 +69,9 @@ template :index do
   %head
     %meta{ 'http-equiv' => 'content', :content => 'text/html; charset=utf-8'}
     %title=@title
-  %body{:style=>"background-image: url(/back.jpg);"}
+  %body{:style=>"background-image: url(/back.jpg); font-family:san-selif;"}
     %div{:style=>"width:800px;margin:auto;"}
-      %h1{:style=>"margin:15px;"}=@title
+      %h1{:style=>"margin:20px; font-size:5em;"}=@title
       - @result.each do |i|
         - unless i[1] == ""
           %div{:style=>"background-image: url(/white80.png); margin:15px; padding:15px; border:1px solid black; border-radius:10px; -webkit-border-radius: 10px; -moz-border-radius:10px;"}
@@ -62,7 +80,7 @@ template :index do
               %p{:style=>"color:black;"}= i[0]
               %pre{:style=>"color:black;"}= i[2]
               - unless i[4] == ""
-                %img{:src=>i[4], :style=>"max-height:400px; max-width:400px;"}
+                %img{:src=>i[4], :style=>"max-height:500px; max-width:500px;"}
 EOF
 end
 
